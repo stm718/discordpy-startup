d922198d234040af026f47839eb7fbcfb4c5f5a0
stm718 <shu.tanaka298@gmail.com>
2020-04-09 21:53:26 +0900

    discordbot.py追記

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,46 @@
-from discord.ext import commands
-import os
-import traceback
+# from discord.ext import commands
+# import os
+# import traceback
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# bot = commands.Bot(command_prefix='/')
+# token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+# @bot.event
+# async def on_command_error(ctx, error):
+#     orig_error = getattr(error, "original", error)
+#     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+#     await ctx.send(error_msg)
 
 
-bot.run(token)
+# @bot.command()
+# async def ping(ctx):
+#     await ctx.send('pong')
+
+
+# bot.run(token)
+
+# インストールした discord.py を読み込む
+import discord
+
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'Njk3NzQ5ODA0NjMzMjkyODYw.Xo8UAg.sE_0ZLFOCB4AbOakxOJ16bkrGBE'
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    if client.user in message.mentions: # 話しかけられたかの判定
+        reply = f'{message.author.mention} 百合はいいぞ' # 返信メッセージの作成
+        await message.channel.send(reply) # 返信メッセージを送信
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
